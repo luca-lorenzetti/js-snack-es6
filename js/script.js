5dda3318a36650dd7c97b79b64990217116e716e
Luca <luca.lorenzetti0@gmail.com>
2021-03-19 16:09:11 +0100

    Sistemato snack bici

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
-$(function(){
-    /* 
+$(function () {
+
+    // 1° Snack
+    function snackBici() {
+        /* 
     Snack 1
     
     Creare un array di oggetti:
@@ -7,46 +10,49 @@ $(function(){
     Stampare a schermo la bici con peso minore utilizzando destructuring e template literal */
 
 
-    //1° Snack
+        //1° Snack
+        console.log(`PRIMO SNACK ( ESERCIIO BICI )
+    `);
 
-    console.log(`PRIMO SNACK ( ESERCIIO BICI )
-                `);
+        const bici = [{
+                nome: "Alan",
+                peso: 5000
+            },
+            {
+                nome: "Atala",
+                peso: 5500
+            },
+            {
+                nome: "Pegoretti",
+                peso: 3000
+            },
+            {
+                nome: "Werking",
+                peso: 4500
+            }
+        ];
 
-    let bici = [
-        {
-            nome: "Alan",
-            peso: 5000
-        },
-        {
-            nome: "Atala",
-            peso: 5500
-        },
-        {
-            nome: "Pegoretti",
-            peso: 3000
-        },
-        {
-            nome: "Werking",
-            peso: 4500
+
+        let minBici = bici[0];
+
+        console.log(`LISTA BICI
+    
+    `);
+        for (let i = 0; i < bici.length; i++) {
+            console.log(`Nome: ${bici[i].nome} - Peso: ${bici[i].peso}`);
+            if (bici[i].peso < minBici.peso) {
+                minBici = bici[i];
+            }
         }
-    ];
 
+        const {
+            nome,
+            peso
+        } = minBici;
 
-    let minBici = bici[0];
-
-    console.log(`LISTA BICI
-                
-                `);
-    for (let i = 0; i < bici.length; i++) {
-        console.log(`Nome: ${bici[i].nome} - Peso: ${bici[i].peso}`);
-        if( bici[i].peso  < minBici.peso){
-            minBici = bici[i];
-        }
+        console.log(`
+La bici più leggera è la bici ${nome} con un peso di ${peso}`);
     }
 
-    const {nome,peso} = minBici;
-
-    console.log(`
-La bici più leggera è la bici ${nome} con un peso di ${peso}`);
-
+    snackBici();
 });
